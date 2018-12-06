07949f5100ef6c29880ed16420eb8c86f7911329
wyq-pc <wangyaqi.chn@gmail.com>
2018-12-06 17:59:28 +0800

    token

--- controller/qiniu.js
+++ controller/qiniu.js
@@ -1,8 +1,15 @@
+const qiniu = require('qn')
+const config = require('../app.config')
+
+console.log(config.qiniu)
+const client = qiniu.create(config.qiniu)
+
 module.exports = {
   getToken(ctx) {
+
     ctx.send({
       code: 1,
-      reuslt: 'token',
+      result: client.uploadToken(),
       message: 'upToken获取成功'
     })
   }

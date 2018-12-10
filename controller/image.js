94fe435a1dfc38871219ebd1df8c51a91560aa17
wyq-pc <wangyaqi.chn@gmail.com>
2018-12-10 15:39:23 +0800

    上传图片

--- controller/image.js
+++ controller/image.js
@@ -8,5 +8,10 @@ module.exports = {
       result,
       message: '添加图片成功'
     })
+  },
+  async getList(ctx) {
+    const { article } = ctx.request.query
+    const result = await Image.find({ article })
+    ctx.send({ code: 1, result, message: '获取图片列表成功' })
   }
 }
--- router/routes/image.js
+++ router/routes/image.js
@@ -2,4 +2,5 @@ const ImageController = require('../../controller/image')
 
 module.exports = (router) => {
   router.post('/image', ImageController.postItem)
+  router.get('/image', ImageController.getList)
 }

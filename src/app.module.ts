2ad2eda67f238e148f31656f86e5c55fe9bf39b9
wangyaqi <wangyaqi.chn@foxmail.com>
2019-08-27 15:31:05 +0800

    add: category

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module'
-import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleModule } from './article/article.module'
+import { CategoryModule } from './category/category.module'
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test', { useNewUrlParser: true }),
     UserModule,
     AuthModule,
-    ArticleModule
+    ArticleModule,
+    CategoryModule
   ]
 })
 
---
+++ src/category/category.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common'
+import { CategoryController } from './category.controller'
+import { CategoryService } from './category.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { CategorySchema } from './schemas/category.schema'
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }])
+  ],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+  exports: [CategoryService]
+})
+export class CategoryModule {}

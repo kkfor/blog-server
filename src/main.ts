6dbb5e189165234f040d131377bf2921462a89d5
wyq-mac <wangyaqi.chn@gmail.com>
2019-08-31 21:55:22 +0800

    update: origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://kkfor.com', 'https://admin.kkfor.com'],
+    methods: "GET,HEAD,PUT,POST,DELETE",
+    credentials: true
+  })
   app.useGlobalFilters(new HttpExceptionFilter())
 
   await app.listen(5000)
